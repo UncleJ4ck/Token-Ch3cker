cd75330628b0ace95668fda5d8073adba1118d58
UncleJ4ck <uncleja1ck@protonmail.com>
2023-01-15 19:20:43 +0100

    fix: del a file

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import requests
 import json
 import time
 
-# Create a function to run a query on the Uniswap GraphQL API
+# function to run a query on the Uniswap GraphQL API
 def run_query(q):
     request = requests.post('https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2', json={'query': q})
     if request.status_code == 200:
@@ -12,7 +12,7 @@ def run_query(q):
         raise Exception('Query failed. Return code is {}'.format(request.status_code))
 
 
-# Create a function to use a honeypot ethereum checker
+# function to use a honeypot ethereum checker
 def check_honeypot(contract_address):
     r = requests.get(f'https://honeypot.is/ethereum?address={contract_address}')
     if r.status_code == 200:
@@ -25,7 +25,7 @@ def check_honeypot(contract_address):
     else:
         raise Exception(f'Error checking contract {contract_address}. Return code is {r.status_code}')
 
-# Create a function to query Etherscan for the source code of a contract
+# function to query Etherscan for the source code of a contract
 def query_etherscan(contract_address):
     request = requests.get(f'https://api.etherscan.io/api?module=contract&action=getsourcecode&address={contract_address}&apikey=961XF454VCYXBHA4RGTR812XI2R2GFUSJW')
     if request.status_code == 200:
@@ -33,28 +33,28 @@ def query_etherscan(contract_address):
     else:
         raise Exception('Query failed. Return code is {}'.format(request.status_code))
 
-# Create a function to check if a given list of functions exists in the source code of a contract
+# function to check if a given list of functions exists in the source code of a contract
 def check_exist_function(source_code, function_list):
     for function in function_list:
         if function in source_code:
             return False
     return True
 
-# Create a function to check if a given token already exists in a list of tokens
+# function to check if a given token already exists in a list of tokens
 def check_existing_token_in_list(token, token_list):
     for t in token_list:
         if t['token'] == token:
             return True
     return False
 
-# Create a function to check if a contract is a Uniswap V2 contract
+# function to check if the contract is a Uniswap V2 contract
 def check_holders(contract_address):
     r = requests.get(f"https://etherscan.io/token/generic-tokenholders2?a={contract_address}&s=0&p=", headers={'User-Agent': 'Mozilla/5.0'})
     html = r.text
     if 'Public Tag: Uniswap V2' in html or 'Public Tag: Null Address' in html:
         return True
 
-# Create a function to get the latest added pairs to the Uniswap network
+# function to get the latest added pairs to the Uniswap network
 def get_new_tokens():
     query = """
     {
@@ -108,6 +108,7 @@ while True:
                     print(f'Added new scam {token_name}:{symbol} ({contract_address}) to the existing list.\n')
         else:
             print(f'Token {token_name}:{symbol} ({contract_address}) already exists in the list.\n')
+        # Saving the output in json files
         with open('tokens.json', 'w') as fp:
             json.dump(existing_tokens, fp, indent=4)
         with open('scams.json', 'w') as fp:
